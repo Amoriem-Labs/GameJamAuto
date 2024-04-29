dd05d30110ffe0233ad53194d9ebf251143c2b0f
Jeffrey Tan <60846941+tanj85@users.noreply.github.com>
2024-04-28 20:57:32 -0400

    UI and Game setup stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,9 +7,22 @@ public class GameManager : MonoBehaviour
     private static GameManager _instance;
     public static GameManager Instance { get { return _instance; } }
 
+    public delegate void HeroStateUpdate();
+
+    public static event HeroStateUpdate onHeroStateUpdated;
+
+    public List<GameObject> allSpells = new List<GameObject>();
+
     public Board board;
     public bool battleOngoing = true;
     public int coins;
+
+    public float heroHealth;
+    public float heroMaxHealth;
+    public float maxMana;
+
+    public List<BaseSpell> playerGrimoire = new List<BaseSpell>();
+
     void Awake()
     {
         if (_instance != null && _instance != this)
@@ -21,5 +34,23 @@ public class GameManager : MonoBehaviour
             _instance = this;
             DontDestroyOnLoad(_instance);
         }
+    }
+
+    // for now just implement a base deck, maybe expand upon this some other time
+    public void startGame()
+    {
+        updateHeroMaxHealth(1000);
+        updateHeroHealth(heroMaxHealth);
+    }
+    public void updateHeroHealth(float _health)
+    {
+        heroHealth = _health;
+        onHeroStateUpdated?.Invoke();
+    }
+
+    public void updateHeroMaxHealth(float _maxHealth)
+    {
+        heroMaxHealth = _maxHealth;
+        onHeroStateUpdated?.Invoke();
     }
 }
---
+++ Assets/Scripts/Gameplay/Spells/BaseSpell.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseSpell : MonoBehaviour
+{
+    public Game gameState;
+
+    public float manaCost = 0;
+    public string spellName = "Unnamed Spell";
+    public string description = "Undefined";
+
+    protected virtual void Start()
+    {
+        manaCost = 10;
+        gameState = GameManager.Instance.GetComponent<Game>();
+        spellName = "Base Spell";
+        description = "This is the base spell. You should not be seeing this >:(";
+    }
+
+    public abstract bool play();
+
+    public abstract List<Tile> highlight(Tile tile);
+
+    public string getDescription()
+    {
+        return "Mana Cost: " + manaCost.ToString("0") + "\n" + description;
+    }
+}
